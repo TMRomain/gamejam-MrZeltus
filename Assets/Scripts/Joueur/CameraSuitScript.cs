2e91b31eb8f64d0d1d3279e0a37a5ab8f8050b7d
Romain <romainover51@gmail.com>
2022-03-15 17:34:37 +0100

    Script qui fait en sorte que la camera suive le joueur #2

---
+++ Assets/Scripts/Joueur/CameraSuitScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSuitScript : MonoBehaviour
+{
+    [SerializeField]
+    Transform joueur;
+
+
+    [SerializeField]
+    Vector3 offset;
+    [SerializeField]
+    float delais = 0.5f;
+    [SerializeField]
+    float vitesse = 5f;
+    private void FixedUpdate() {
+        transform.position = Vector3.Lerp(transform.position ,new Vector3(joueur.position.x,joueur.position.y,transform.position.z), Time.deltaTime * vitesse);
+    }
+}
